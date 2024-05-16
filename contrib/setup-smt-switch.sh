fac839ab05bbcbbf7117d55eecf40cb2bb2c5764
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2024-05-16 07:01:16 -0700

    Do not use --static for smt-switch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -76,7 +76,7 @@ if [ ! -d "$DEPS/smt-switch" ]; then
         ./contrib/setup-bitwuzla.sh
     fi
     # pass bison/flex directories from smt-switch perspective
-    ./configure.sh --btor --cvc5 $CONF_OPTS --prefix=local --static --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install
+    ./configure.sh --btor --cvc5 $CONF_OPTS --prefix=local --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install
     cd build
     make -j$(nproc)
     make test
