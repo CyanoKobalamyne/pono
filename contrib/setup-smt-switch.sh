95000627cbbe7ef2f86aa9fda7177dffbf441da8
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2024-05-16 07:02:38 -0700

    Override libgmp search procedure to include static lib

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -67,7 +67,8 @@ if [ ! -d "$DEPS/smt-switch" ]; then
         ./contrib/setup-cvc5.sh
     fi
     # pass bison/flex directories from smt-switch perspective
-    ./configure.sh --btor --cvc5 $CONF_OPTS --prefix=local --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install
+    # override included CMake modules with our own so that static libgmp is linked
+    ./configure.sh --btor --cvc5 $CONF_OPTS --prefix=local --smtlib-reader --bison-dir=../bison/bison-install --flex-dir=../flex/flex-install -DCMAKE_MODULE_PATH="$DIR/../cmake"
     cd build
     make -j$(nproc)
     make test
